19cd8ff11d6a74af2901231edfc6d630d059eb06
Thomas <56854034+Marcentus@users.noreply.github.com>
2026-06-07 21:13:49 -0400

    Possible fix for network packet loss detected errors. Added in tcp sequence wrap around

--- packet/gamePacketReader_test.go
+++ packet/gamePacketReader_test.go
@@ -76,3 +76,37 @@ func TestParsedPacketDeduperAllowsDifferentPacketsAcrossFlows(t *testing.T) {
 		t.Fatal("different parsed packets across flows should be allowed")
 	}
 }
+
+func TestCircularArithmetic(t *testing.T) {
+	// Old sequence number near the wrap-around point (e.g. from your log)
+	var oldSeq uint32 = 4293934746
+	// New sequence number wrapped around to a small number
+	var newSeq uint32 = 446430
+
+	// Unsigned comparison (which was buggy and caused the parser to fail)
+	if !(oldSeq > newSeq) {
+		t.Error("unsigned oldSeq should be greater than newSeq")
+	}
+
+	// Correct circular comparison: oldSeq is actually BEFORE newSeq
+	// (i.e. oldSeq < newSeq in TCP circular sequence space)
+	if int32(oldSeq-newSeq) >= 0 {
+		t.Error("oldSeq should be before newSeq in circular sequence space")
+	}
+
+	if int32(newSeq-oldSeq) <= 0 {
+		t.Error("newSeq should be after oldSeq in circular sequence space")
+	}
+
+	// Test a normal non-wrapped sequence pair
+	var seqA uint32 = 1000
+	var seqB uint32 = 1500
+
+	if int32(seqA-seqB) >= 0 {
+		t.Error("seqA should be before seqB in normal sequence space")
+	}
+	if int32(seqB-seqA) <= 0 {
+		t.Error("seqB should be after seqA in normal sequence space")
+	}
+}
+
